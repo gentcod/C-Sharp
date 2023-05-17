262858c5938d9a2fd21e332a69d6941bde8aa045
Oyefule <drelanorgent@gmail.com>
2023-05-17 10:19:03 +0100

    Code correction

--- Calculator/Calc.cs
+++ Calculator/Calc.cs
@@ -2,68 +2,68 @@ namespace ConsoleCalculator;
 
 public class Calc
 {
-    public int firstOperand;
-    public int secondOperand;
-    private readonly List<string> _mathOperation = new List<string> { "add", "subtract", "divide", "multiply" };
+   public int firstOperand;
+   public int secondOperand;
+   private readonly List<string> _mathOperation = new List<string> { "add", "subtract", "divide", "multiply" };
 
-    //public Calc(int firstNum, int secondNum, string mathOp) 
-    public Calc() {}
+   //public Calc(int firstNum, int secondNum, string mathOp) 
+   public Calc() { }
 
-    private int Add(int operand1, int operand2)
-    {
-        return operand1 + operand2;
-    }
+   public int Add(int operand1, int operand2)
+   {
+      return operand1 + operand2;
+   }
 
-    private int Subtract(int operand1, int operand2)
-    {
-        return operand1 - operand2;
-    }
+   public int Subtract(int operand1, int operand2)
+   {
+      return operand1 - operand2;
+   }
 
-    private int Multiply(int operand1, int operand2)
-    {
-        return operand1 * operand2;
-    }
+   public int Multiply(int operand1, int operand2)
+   {
+      return operand1 * operand2;
+   }
 
-    private int Divide(int operand1, int operand2)
-    {
-        return operand1 / operand2;
-    }
+   public int Divide(int operand1, int operand2)
+   {
+      return operand1 / operand2;
+   }
 
-    public void Calculate(int operand1, int operand2, string operation)
-    {
-        firstOperand = operand1;
-        secondOperand = operand2;
+   public void Calculate(int operand1, int operand2, string operation)
+   {
+      firstOperand = operand1;
+      secondOperand = operand2;
 
-        //Initialize answer variable
-        int ans = 0;
+      //Initialize answer variable
+      int ans = 0;
 
-        if (operation != null)
-        {
-            string opr = operation.ToLower();
+      if (operation != null)
+      {
+         string opr = operation.ToLower();
 
-            if (_mathOperation.Contains(opr))
-            {
-                if (opr == "add")
-                    ans = Add(firstOperand, secondOperand);
+         if (_mathOperation.Contains(opr))
+         {
+            if (opr == "add")
+               ans = Add(firstOperand, secondOperand);
 
-                if (opr == "subtract")
-                    ans = Subtract(firstOperand, secondOperand);
+            if (opr == "subtract")
+               ans = Subtract(firstOperand, secondOperand);
 
-                if (opr == "multiply")
-                    ans = Multiply(firstOperand, secondOperand);
+            if (opr == "multiply")
+               ans = Multiply(firstOperand, secondOperand);
 
-                if (opr == "divide")
-                    ans = Divide(firstOperand, secondOperand);
+            if (opr == "divide")
+               ans = Divide(firstOperand, secondOperand);
 
-                Console.WriteLine($"The answer is {ans}");
-            }
+            Console.WriteLine($"The answer is {ans}");
+         }
 
-            else
-            {
-                Console.WriteLine("This operation is wrong, run the application again");
-                return;
-            }
-        }
+         else
+         {
+            Console.WriteLine("This operation is wrong, run the application again");
+            return;
+         }
+      }
     }
 
 }
